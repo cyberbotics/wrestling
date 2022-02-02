692af699e6e2a1d99a757be7c35c8f22aba667b6
Olivier Michel <Olivier.Michel@cyberbotics.com>
2022-02-02 15:31:47 +0100

    wresting simulation

---
+++ controllers/wrestler_red/wrestler_red.py
@@ -0,0 +1,76 @@
+# Copyright 1996-2022 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Controller example for the Robot Wrestling Tournament.
+   Demonstrates how to access sensors and actuators"""
+
+from controller import Robot, Motion
+
+
+class Wrestler (Robot):
+    def __init__(self):
+        Robot.__init__(self)
+        self.timeStep = int(self.getBasicTimeStep())  # retrieves the WorldInfo.basicTimeTime (ms) from the world file
+
+        # camera
+        self.cameraTop = self.getDevice("CameraTop")
+        self.cameraBottom = self.getDevice("CameraBottom")
+        self.cameraTop.enable(4 * self.timeStep)
+        self.cameraBottom.enable(4 * self.timeStep)
+
+        # there are 7 controlable LEDs on the NAO robot, but we will use only the ones in the eyes
+        self.leds = []
+        self.leds.append(self.getDevice('Face/Led/Right'))
+        self.leds.append(self.getDevice('Face/Led/Left'))
+
+        # shoulder pitch motors
+        self.RShoulderPitch = self.getDevice("RShoulderPitch")
+        self.LShoulderPitch = self.getDevice("LShoulderPitch")
+
+        # load motion files
+        self.forwards = Motion('motions/Forwards50.motion')
+        self.turnLeft60 = Motion('motions/TurnLeft60.motion')
+
+    def run(self):
+        self.RShoulderPitch.setPosition(1.57)  # arms down
+        self.LShoulderPitch.setPosition(1.57)
+
+        self.forwards.setLoop(True)
+        self.forwards.play()
+
+        self.leds[0].set(0xff0000)  # set eyes to red
+        self.leds[1].set(0xff0000)
+
+        while self.step(self.timeStep) != -1:
+            t = self.getTime()
+            if t == 22:
+                self.forwards.stop()
+                self.turnLeft60.play()
+                self.RShoulderPitch.setPosition(0)  # upercut
+            elif t == 25:
+                self.turnLeft60.stop()
+                self.forwards.play()  # push
+            elif t == 27:
+                self.forwards.stop()  # stop
+            elif t == 28:
+                self.LShoulderPitch.setPosition(-1.57)  # victory
+                self.RShoulderPitch.setPosition(-1.57)
+                self.leds[0].set(0x00ff00)  # set eyes to green
+                self.leds[1].set(0x00ff00)
+            pass
+
+
+# create the Robot instance and run main loop
+wrestler = Wrestler()
+wrestler.run()
---
+++ controllers/wrestling_referee/wrestling_referee.py
@@ -0,0 +1,107 @@
+# Copyright 1996-2019 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Referee supervisor controller for the Robot Wrestling Tournament."""
+
+import math
+from controller import Supervisor
+
+
+class Referee (Supervisor):
+    def init(self):
+        self.digit = [[0] * 10 for i in range(3)]  # create an array of size [3][10] filled in with zeros
+        for j in range(3):
+            for i in range(10):
+                self.digit[j][i] = self.getDevice("digit " + str(j) + str(i))
+        self.currentDigit = [0, 0, 0]  # 0:00
+        self.robot = [0] * 2
+        self.robot[0] = self.getFromDef("WRESTLER_RED")
+        self.robot[1] = self.getFromDef("WRESTLER_BLUE")
+        self.min = [[0] * 3 for i in range(2)]
+        self.max = [[0] * 3 for i in range(2)]
+        for i in range(2):
+            self.min[i] = self.robot[i].getPosition()
+            self.max[i] = self.robot[i].getPosition()
+        self.coverage = [0] * 2
+        self.koCount = [0] * 2
+        self.indicator = [0] * 2
+        self.indicator[0] = self.getDevice("red indicator")
+        self.indicator[1] = self.getDevice("blue indicator")
+
+    def displayTime(self, minutes, seconds):
+        for j in range(3):
+            self.digit[j][self.currentDigit[j]].setPosition(1000)  # far away, not visible
+        self.currentDigit[0] = minutes
+        self.currentDigit[1] = seconds // 10
+        self.currentDigit[2] = seconds % 10
+        for j in range(3):
+            self.digit[j][self.currentDigit[j]].setPosition(0)  # visible
+
+    def run(self):
+        matchDuration = 3 * 60 * 1000  # a match lasts 3 minutes
+        timeStep = int(self.getBasicTimeStep())  # retrieves the WorldInfo.basicTimeTime (ms) from the world file
+        time = 0
+        seconds = -1
+        ko = -1
+        while True:
+            if time % 200 == 0:
+                s = int(time / 1000) % 60
+                if seconds != s:
+                    seconds = s
+                    minutes = int(time / 60000)
+                    self.displayTime(minutes, seconds)
+                box = [0] * 3
+                for i in range(2):
+                    position = self.robot[i].getPosition()
+                    if abs(position[0]) > 1 or abs(position[1]) > 1:  # outside of the ring
+                        continue
+                    coverage = 0
+                    for j in range(3):
+                        if position[j] < self.min[i][j]:
+                            self.min[i][j] = position[j]
+                        elif position[j] > self.max[i][j]:
+                            self.max[i][j] = position[j]
+                        box[j] = self.max[i][j] - self.min[i][j]
+                        coverage += box[j] * box[j]
+                    coverage = math.sqrt(coverage)
+                    self.coverage[i] = coverage
+                    self.indicator[i].setPosition(self.coverage[i] / 7)
+                if position[1] < 0.75:  # low position threshold
+                    self.koCount[i] = self.koCount[i] + 200
+                    if self.koCount[i] > 10000:  # 10 seconds
+                        ko = i
+                else:
+                    self.koCount[i] = 0
+                if self.koCount[0] > self.koCount[1]:
+                    print("\fred KO: %d" % (10 - self.koCount[0] // 1000))
+                elif self.koCount[1] > self.koCount[0]:
+                    print("\fblue KO: %d" % (10 - self.koCount[1] // 1000))
+                #  print("\fred: %1.3f - blue: %1.3f" % (self.coverage[0], self.coverage[1]))
+            if self.step(timeStep) == -1 or time > matchDuration or ko != -1:
+                break
+            time += timeStep
+        if ko == 0:
+            print("Wrestler red is KO. Wrestler blue wins!")
+        elif ko == 1:
+            print("Wrestler blue is KO. Wrestler red wins!")
+        elif self.coverage[0] >= self.coverage[1]:  # in case of coverage equality, red wins
+            print("Wresler red wins: %s >= %s" % (self.coverage[0], self.coverage[1]))
+        else:
+            print("Wresler blue wins: %s > %s" % (self.coverage[1], self.coverage[0]))
+
+
+# create the referee instance and run main loop
+referee = Referee()
+referee.init()
+referee.run()
