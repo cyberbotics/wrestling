aacdda306c2019be1faa3dbcb5e600faf66a40f4
Léo Duggan <leo.duggan@alumni.epfl.ch>
2023-01-20 09:10:45 +0100

    Added robot window for the competition

---
+++ plugins/robot_windows/competition_description/competition_description.js
@@ -0,0 +1,15 @@
+import RobotWindow from 'https://cyberbotics.com/wwi/R2023a/RobotWindow.js';
+
+window.robotWindow = new RobotWindow();
+
+window.robotWindow.receive = function(message, robot) {
+  if (message.startsWith('data:image')) {
+    const imageElement = document.getElementById('robot-camera');
+    if (imageElement != null)
+      imageElement.setAttribute('src', message);
+  } else {
+    if (message.length > 200)
+      message = message.substr(0, 200);
+    console.log("Received unknown message for robot '" + robot + "': '" + message + "'");
+  }
+};
